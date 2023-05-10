b35ae70f4857d0d4ae1460f142fba7ddf77aad5e
Miaad <Miaad@MIAAD>
2023-05-10 22:05:01 +0330

    Added Entity Core DbContext

---
+++ Hogwarts_MVVM/Hogwarts.Core/Data/HogwartsDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Hogwarts.Core.Models.Authentication;
+using Hogwarts.Core.Models.CourseManagement;
+using Hogwarts.Core.Models.DormitoryManagement;
+using Hogwarts.Core.Models.FacultyManagement;
+using Hogwarts.Core.Models.Forest;
+using Hogwarts.Core.Models.StudentManagement;
+using Hogwarts.Core.Models.TrainManagement;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Configuration;
+
+namespace Hogwarts.Core.Data
+{
+    public class HogwartsDbContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Dormitory> Dormitories { get; set; }
+        public DbSet<ActivationCode> ActivationCodes { get; set; }
+        public DbSet<Plant> Plants { get; set; }
+        public DbSet<Train> Trains { get; set; }
+
+
+        private static string GetConnectionString()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            return config.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(GetConnectionString());
+        }
+    }
+}
