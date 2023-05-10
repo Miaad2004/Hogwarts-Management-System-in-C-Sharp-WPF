8ea7c8c4004568f65746a5d1cbe5dc5960ae36f8
Miaad <Miaad@MIAAD>
2023-05-10 22:13:11 +0330

    Added App.xaml

---
+++ Hogwarts_MVVM/Hogwarts_MVVM/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Hogwarts.ViewModels;
+using System.Windows;
+
+namespace Hogwarts_MVVM
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+    }
+}
