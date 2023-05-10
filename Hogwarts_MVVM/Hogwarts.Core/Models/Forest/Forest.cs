924c55debbbba8e71938069f85023270a0e99dd8
Miaad <Miaad@MIAAD>
2023-05-10 22:06:29 +0330

    Added Forest

---
+++ Hogwarts_MVVM/Hogwarts.Core/Models/Forest/Forest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hogwarts.Core.Data;
+
+namespace Hogwarts.Core.Models.Forest
+{
+    public class Forest
+    {
+        private readonly HogwartsDbContext _context;
+
+        public Forest(HogwartsDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public void AddPlant(Plant plant, int addQuantity)
+        {
+            if (plant == null)
+            {
+                throw new ArgumentNullException(nameof(plant));
+            }
+            if (addQuantity <= 0)
+            {
+                throw new ArgumentException("Quantity must be a positive integer greater than zero.", nameof(addQuantity));
+            }
+
+            var existingPlant = _context.Plants.FirstOrDefault(p => p.Name == plant.Name);
+            if (existingPlant != null)
+            {
+                existingPlant.Quantity += addQuantity;
+            }
+            else
+            {
+                plant.Quantity = addQuantity;
+                _context.Plants.Add(plant);
+            }
+
+            _context.SaveChanges();
+        }
+
+        public void UpdatePlant(Plant plant, int newQuantity)
+        {
+            if (plant == null)
+            {
+                throw new ArgumentNullException(nameof(plant));
+            }
+            if (newQuantity <= 0)
+            {
+                throw new ArgumentException("Quantity must be a positive integer greater than zero.", nameof(newQuantity));
+            }
+
+            var existingPlant = _context.Plants.FirstOrDefault(p => p.Name == plant.Name);
+            if (existingPlant == null)
+            {
+                throw new KeyNotFoundException($"The plant {plant.Name} is not in the forest.");
+            }
+
+            existingPlant.Quantity = newQuantity;
+
+            _context.SaveChanges();
+        }
+
+        public void RemovePlant(Plant plant)
+        {
+            if (plant == null)
+            {
+                throw new ArgumentNullException(nameof(plant));
+            }
+
+            var existingPlant = _context.Plants.FirstOrDefault(p => p.Name == plant.Name);
+            if (existingPlant == null)
+            {
+                throw new KeyNotFoundException($"The plant {plant.Name} is not in the forest.");
+            }
+
+            _context.Plants.Remove(existingPlant);
+
+            _context.SaveChanges();
+        }
+
+        public int GetPlantQuantity(Plant plant)
+        {
+            if (plant == null)
+            {
+                throw new ArgumentNullException(nameof(plant));
+            }
+
+            var existingPlant = _context.Plants.FirstOrDefault(p => p.Name == plant.Name);
+            if (existingPlant == null)
+            {
+                throw new KeyNotFoundException($"The plant {plant.Name} is not in the forest.");
+            }
+
+            return existingPlant.Quantity;
+        }
+
+        public List<Plant> GetAllPlants()
+        {
+            return _context.Plants.ToList();
+        }
+    }
+}
---
+++ Hogwarts_MVVM/Hogwarts.Core/Models/Forest/Plant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hogwarts.Core.Models.Forest
+{
+    public class Plant
+    {
+        [Key]
+        public string Name {  get; private set; }
+        public string? Description { get; private set; }
+        public DateTime PlantingTime { get; private set; }
+        public TimeSpan GrowthTimeSpan { get; private set; }
+        public DateTime HarvestTime => PlantingTime + GrowthTimeSpan;
+        public bool IsCollectable => HarvestTime > DateTime.Now;
+
+        private int _quantity;
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Quantity must be positive", nameof(value));
+                }
+                _quantity = value; 
+            }
+        }
+
+        public Plant(string name, DateTime plantingTime, TimeSpan growthTimeSpan, string? description = null)
+            : base()
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("The name of the plant cannot be empty.", nameof(name));
+            }
+            if (PlantingTime > DateTime.Now)
+            {
+                throw new ArgumentException("PlantingTime is not valid.");
+            }
+
+            Name = name;
+            Description = description;
+            PlantingTime = plantingTime;
+            GrowthTimeSpan = growthTimeSpan;
+        }
+    }
+}
