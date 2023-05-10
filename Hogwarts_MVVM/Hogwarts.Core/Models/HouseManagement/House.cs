379534b2b08186d55443d1ba5e7d77fc3f19abc5
Miaad <Miaad@MIAAD>
2023-05-10 22:08:14 +0330

    Added House management

---
+++ Hogwarts_MVVM/Hogwarts.Core/Models/HouseManagement/House.cs
@@ -0,0 +1,44 @@
+﻿using Hogwarts.Core.Models.FacultyManagement;
+using Hogwarts.Core.Models.StudentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts.Core.Models.HouseManagement
+{
+    public class House: Entity
+    {
+        public House HouseType { get; private set; }
+
+        private int _score;
+
+        public int Score
+        {
+            get { return _score; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Score must be a non-negative integer.");
+                }
+                _score = value;
+            }
+        }
+
+
+        public IEnumerable<Student> Students { get; private set; }
+
+        public Professor Head { get; private set; }
+
+        public House(House houseType, Professor head)
+            : base()
+        {
+            HouseType = houseType;
+            Score = _score;
+            Head = head;
+            Students = new List<Student>();
+        }
+    }
+}
---
+++ Hogwarts_MVVM/Hogwarts.Core/Models/HouseManagement/Services/HouseService.cs
@@ -0,0 +1,9 @@
+﻿namespace Hogwarts.Core.Models.HouseManagement.Services
+{
+    public class HouseService
+    {
+        // method to add student 
+        // use dependency injection for dbcontext
+        // method to give/ drop points
+    }
+}
