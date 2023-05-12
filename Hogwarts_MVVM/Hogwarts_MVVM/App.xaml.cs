c4ce63d72faf37a0692214b6fa9a81e55ec99b44
Miaad <Miaad@MIAAD>
2023-05-12 15:39:35 +0330

    Automated the migrations

--- Hogwarts_MVVM/Hogwarts_MVVM/App.xaml.cs
+++ Hogwarts_MVVM/Hogwarts_MVVM/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Hogwarts.ViewModels;
+﻿using Hogwarts.Core.Models.Authentication.DTOs;
+using Hogwarts.Core.Models.Authentication.Exceptions;
+using Hogwarts.Core.SharedServices;
+using Newtonsoft.Json;
+using System.IO;
 using System.Windows;
 
 namespace Hogwarts_MVVM
@@ -8,11 +12,25 @@ namespace Hogwarts_MVVM
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            MainWindow = new MainWindow()
+            _ = StaticServiceProvidor.DbContext.Database.EnsureCreated();
+
+            // Register the first admin
+            const string ADMIN_DTO_PATH = "admin.json";
+            if (!File.Exists(ADMIN_DTO_PATH))
             {
-                DataContext = new MainViewModel()
-            };
-            MainWindow.Show();
+                throw new FileNotFoundException($"Could not find {ADMIN_DTO_PATH}");
+            }
+
+            try
+            {
+                string json = File.ReadAllText(ADMIN_DTO_PATH);
+                AdminRegistrationDTO DTO = JsonConvert.DeserializeObject<AdminRegistrationDTO>(json);
+                StaticServiceProvidor.authenticationService.SignUpAdmin(DTO);
+            }
+            catch (UserExistsException)
+            {
+                // Admin already exists
+            }
         }
 
         protected override void OnExit(ExitEventArgs e)
