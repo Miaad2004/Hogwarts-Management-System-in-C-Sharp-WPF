46d402128c5554f5f4ba1c883c4099fc91608ea2
Miaad <Miaad@MIAAD>
2023-05-14 13:50:26 +0330

    Added admin's dorm view

---
+++ Hogwarts_MVVM/Hogwarts_MVVM/AdminViews/Popups/AddDormitoryPopup.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hogwarts.Views.AdminViews.Popups
+{
+    /// <summary>
+    /// Interaction logic for AddDormitoryPopup.xaml
+    /// </summary>
+    public partial class AddDormitoryPopup : Window
+    {
+        public AddDormitoryPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void AddDormitory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
