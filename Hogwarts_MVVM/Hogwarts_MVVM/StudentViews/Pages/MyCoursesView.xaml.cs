745841645c90f00e3cedb9c45fbd8d90a5511cc9
Miaad <Miaad@MIAAD>
2023-05-19 11:58:02 +0330

    Finished student's MyCourses view

--- Hogwarts_MVVM/Hogwarts_MVVM/StudentViews/Pages/MyCoursesView.xaml.cs
+++ Hogwarts_MVVM/Hogwarts_MVVM/StudentViews/Pages/MyCoursesView.xaml.cs
@@ -1,5 +1,13 @@
-﻿using System;
+﻿using Hogwarts.Core.Models.Authentication;
+using Hogwarts.Core.Models.CourseManagement;
+using Hogwarts.Core.Models.CourseManagement.Exceptions;
+using Hogwarts.Core.Models.DormitoryManagement;
+using Hogwarts.Core.Models.StudentManagement;
+using Hogwarts.Core.SharedServices;
+using Hogwarts.Views.AdminViews.Popups;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +28,64 @@ namespace Hogwarts.Views.StudentViews.Pages
     /// </summary>
     public partial class MyCoursesView : Page
     {
+        private static Guid StudentId => SessionManager.CurrentSession.User.Id;
+        private static ObservableCollection<Course> MyCourses
+        {
+            get
+            {
+                var courses = StaticServiceProvidor.dbContext.GetList<Student>(
+                    orderBy: s => s.FullName,
+                    includeProperties: s => s.Courses,
+                    whereClause: s => s.Id == StudentId
+                ).First().Courses.Where(c => !c.HasFinished).OrderBy(c => c.Title);
+
+                return new ObservableCollection<Course>(courses);
+            }
+        }
+
         public MyCoursesView()
         {
             InitializeComponent();
+            Loaded += OnDataGridChanged;
+        }
+        private void OnDataGridChanged(object sender, RoutedEventArgs e)
+        {
+            myCoursesDataGrid.ItemsSource = MyCourses;
+            myCoursesDataGrid.Items.Refresh();
+        }
+
+        private void ViewScore_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Button button = sender as Button;
+                Course selectedCourse = button.DataContext as Course;
+
+                GradeType grade = StaticServiceProvidor.courseService.GetStudentGrade(StudentId, selectedCourse.Id);
+                MessageBox.Show($"You'r grade is {grade}",
+                                "Success!",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Information);
+            }
+
+            catch (Exception ex)
+            {
+                if (ex is GradeNotRegisteredException)
+                {
+                    MessageBox.Show(ex.Message, "Warning!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+                else if (ex is ArgumentException ||
+                         ex is CourseException)
+                {
+                    MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                else
+                {
+                    throw ex;
+                }
+            }
         }
     }
 }
