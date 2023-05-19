855860c7dffe200a7b2fb24b1e60d084c3ade0bb
Miaad <Miaad@MIAAD>
2023-05-19 11:57:40 +0330

    finished student's home view

--- Hogwarts_MVVM/Hogwarts_MVVM/StudentViews/Pages/HomeView.xaml.cs
+++ Hogwarts_MVVM/Hogwarts_MVVM/StudentViews/Pages/HomeView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hogwarts.Core.Models.Authentication;
+using Hogwarts.Core.SharedServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,27 @@ namespace Hogwarts.Views.StudentViews.Pages
     /// </summary>
     public partial class HomeView : Page
     {
+        private Guid CurrentStudentId => SessionManager.CurrentSession.User.Id;
+
+        private string Username => SessionManager.CurrentSession.User.Username;
+        private string FullName => SessionManager.CurrentSession.User.FullName;
+        private string Age => SessionManager.CurrentSession.User.Age.ToString();
+        private string BloodType => SessionManager.CurrentSession.User.BloodType.ToString();
+        private string Email => SessionManager.CurrentSession.User.Email;
+
         public HomeView()
         {
             InitializeComponent();
+            txtUsername.Text = Username;
+            txtFullName.Text = FullName;
+            txtAge.Text = Age;
+            txtBloodType.Text = BloodType;
+            txtEmail.Text = Email;
+
+            txtActiveCourses.Text = StaticServiceProvidor.courseService.GetActiveCourseCount(CurrentStudentId).ToString();
+            //txtActiveAssignments.Text = StaticServiceProvidor.assignmentService.GetActiveAssignmentCount(CurrentStudentId).ToString();
+            txtHouse.Text = StaticServiceProvidor.houseService.GetHouseType(CurrentStudentId).ToString();
+            txtHousePoints.Text = StaticServiceProvidor.houseService.GetHousePoints(CurrentStudentId).ToString();
         }
     }
 }
