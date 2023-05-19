01fd86940be89a73705b17de43a53bd521cc25fd
Miaad <Miaad@MIAAD>
2023-05-19 11:57:14 +0330

    Update course catalog view

--- Hogwarts_MVVM/Hogwarts_MVVM/StudentViews/Pages/CourseCatalogView.xaml.cs
+++ Hogwarts_MVVM/Hogwarts_MVVM/StudentViews/Pages/CourseCatalogView.xaml.cs
@@ -1,5 +1,13 @@
-﻿using System;
+﻿using Hogwarts.Core.Models.Authentication;
+using Hogwarts.Core.Models.CourseManagement;
+using Hogwarts.Core.Models.TrainManagement.Exceptions;
+using Hogwarts.Core.Models.TrainManagement;
+using Hogwarts.Core.SharedServices;
+using Hogwarts.Core.SharedServices.Exceptions;
+using Hogwarts.Views.ProfessorViews.Popups;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Hogwarts.Core.Models.CourseManagement.Exceptions;
 
 namespace Hogwarts.Views.StudentViews.Pages
 {
@@ -20,29 +29,68 @@ namespace Hogwarts.Views.StudentViews.Pages
     /// </summary>
     public partial class CourseCatalogView : Page
     {
+        private static Guid CurrentStudentId => SessionManager.CurrentSession.User.Id;
+        private static ObservableCollection<Course> Courses
+        {
+            get
+            {
+                var courses = StaticServiceProvidor.dbContext.GetList<Course>(
+                    orderBy: c => c.Title,
+                    includeProperties: c => c.Professor
+                ).ToList().Where(c => !c.HasFinished);
+
+                return new ObservableCollection<Course>(courses);
+            }
+        }
+
         public CourseCatalogView()
         {
             InitializeComponent();
+            Loaded += OnDataGridChanged;
+        }
+        private void OnDataGridChanged(object sender, RoutedEventArgs e)
+        {
+            coursessDataGrid.ItemsSource = Courses;
+            coursessDataGrid.Items.Refresh();
         }
 
         private void AddCourse_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                Button button = sender as Button;
+                Course selectedCourse = button.DataContext as Course;
 
-        }
+                MessageBoxResult choice = MessageBox.Show("Are you certain about enrolling in the course?",
+                                                          "Question!",
+                                                          MessageBoxButton.YesNo,
+                                                          MessageBoxImage.Question);
+                if (choice == MessageBoxResult.Yes)
+                {
+                    StaticServiceProvidor.courseService.EnrollStudentInCourse(CurrentStudentId, selectedCourse.Id);
+                    MessageBox.Show("Course added successfully.",
+                                    "Success!",
+                                    MessageBoxButton.OK,
+                                    MessageBoxImage.Information);
+                }
+            }
 
-        private void SubmitScores_Click(object sender, RoutedEventArgs e)
-        {
+            catch (Exception ex)
+            {
+                if (ex is ArgumentException ||
+                    ex is CourseException)
+                {
+                    MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
 
-        }
+                else
+                {
+                    throw ex;
+                }
+            }
 
-        private void AddAssignment_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void AddCourse_Click_1(object sender, RoutedEventArgs e)
-        {
-
+            // Refresh the page
+            OnDataGridChanged(this, new RoutedEventArgs());
         }
     }
 }
