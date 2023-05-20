49b36dc471aa349c5bdd2a98d7fd4dfcdcb08f36
Miaad <Miaad@MIAAD>
2023-05-20 11:35:17 +0330

    Removed binaries

--- Hogwarts_MVVM/Hogwarts.Core/Models/StudentManagement/Student.cs
+++ Hogwarts_MVVM/Hogwarts.Core/Models/StudentManagement/Student.cs
@@ -1,43 +1,37 @@
 ﻿using Hogwarts.Core.Models.Authentication;
 using Hogwarts.Core.Models.Authentication.DTOs;
+using Hogwarts.Core.Models.CourseManagement;
 using Hogwarts.Core.Models.DormitoryManagement;
+using Hogwarts.Core.Models.ForestManagement;
+using Hogwarts.Core.Models.HouseManagement;
 
 namespace Hogwarts.Core.Models.StudentManagement
 {
     public sealed class Student : User
     {
         public int MaxAllowedCourses { get; private set; } = 4;
+        public House House { get; private set; }
+        public Guid HouseId { get; private set; }
         public HouseType HouseType { get; private set; }
         public bool HasLuggage { get; private set; }
         public PetType Pet { get; private set; }
         public DormitoryRoom DormitoryRoom { get; set; }    
         public Year Year { get; set; }
 
-        public Student() { }
-        public Student(string username, string firstName, string lastName, string email, DateOnly birthDay,
-                     BloodType bloodType, AccessLevels accessLeve, string passwordHash, string profileImagePath,
-                     PetType pet, int maxCourses = 4)
-            : base(username, firstName, lastName, email, birthDay, bloodType, accessLeve, passwordHash, profileImagePath)
-        {
-            Pet = pet;
-            Sort();
-            MaxAllowedCourses = maxCourses;
-            Year = Year.First;
-        }
+        public ICollection<Course> Courses { get; private set; } = new List<Course>();
+        public ICollection<Grade> Grades { get; private set; } = new List<Grade>();
+        public ICollection<StudentPlant> StudentPlants { get; private set; } = new List<StudentPlant>();
+        public ICollection<StudentAssignment> StudentAssignments { get; private set; } = new List<StudentAssignment>();
 
-        public Student(StudentRegistrationDTO DTO, string passwordHash)
+        public Student() { }
+        public Student(StudentRegistrationDTO DTO, string passwordHash, House house)
             : base(DTO, passwordHash)
         {
             HasLuggage = DTO.HasLuggage;
             Pet = DTO.Pet;
-            Sort();
             Year = Year.First;
-        }
-
-        private void Sort()
-        {
-            Random random = new();
-            HouseType = (HouseType)random.Next(0, Enum.GetValues(typeof(HouseType)).Length);
+            House = house;
+            HouseType = house.HouseType;
         }
     }
 }
